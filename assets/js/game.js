311097d7d597d5db554aa60f76dc93469fe30775
Kiefer Levine <kiefer_8@hotmail.com>
2021-04-29 13:47:01 -0400

    Added random values.

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,7 +27,7 @@ var fight = function(enemyRobot) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // Subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       } else {
@@ -36,7 +36,9 @@ var fight = function(enemyRobot) {
     } else if (promptFight.toLowerCase() === "fight") {
   
       //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damageDealt = randomNumber(playerAttack - 3, playerAttack);
+
+      enemyHealth = Math.max(0, enemyHealth - damageDealt);
     
       // Log a resulting message to the console so we know that it worked.
       console.log(
@@ -52,7 +54,9 @@ var fight = function(enemyRobot) {
       }
     
       // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-      playerHealth = playerHealth - enemyAttack;
+      var damageTaken = randomNumber(enemyAttack - 3, enemyAttack);
+
+      playerHealth = Math.max(0, playerHealth - damageTaken);
     
       // Log a resulting message to the console so we know that it worked.
       console.log(
@@ -83,7 +87,7 @@ var startGame = function() {
     if (playerHealth > 0 ) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
 
       // if we're not at the last enemy in the array, shoptime
@@ -165,6 +169,8 @@ var shop = function() {
       break;
   }
 }
+// The random values come from here
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Get da ball rollin'
 startGame();
